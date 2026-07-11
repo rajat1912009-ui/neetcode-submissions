bedf1929688c7de1f05c6a78f53cef545e8332a1
Rajat Aggarwal <rajat1912009@gmail.com>
2026-07-11 11:04:38 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    bool isAnagram(string s, string t) {
+        
+       //Initial length check
+       if(s.length() != t.length())
+       {
+        return false;
+       }
+
+       //declare a fixed size array hash
+       char count[26] = {0};
+
+       //filling/emptying buckets
+       for(int i = 0; i < s.length(); i++)
+       {
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+        }
+
+        //final bucket empty check, if all buckets empty, is a valid anagram
+        for(int i = 0; i < 26; i++)
+        {
+            if (count[i] != 0) {
+                return false;
+            }
+            
+        } return true;
+    }
+};
