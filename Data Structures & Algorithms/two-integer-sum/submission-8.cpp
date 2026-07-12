1c534141b546e4d1dc5350afc1b185907ec0fcca
Rajat Aggarwal <rajat1912009@gmail.com>
2026-07-12 17:17:48 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        //cant use any other type since the input cases are so ####
+
+        std::unordered_map<int, int> nummap;
+
+
+        //find the complement of each indice in nums
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+
+        //find out if complement exists in our hash
+        if (nummap.find(complement) != nummap.end()) {
+            return {nummap[complement], i};
+
+            
+        } 
+        //if it didnt in our previous loop, indice it in the hash map
+        nummap[nums[i]] = i;
+
+        }
+        return {};
+    }
+};
